f7b0bae6da62e20a364aac5e52d60da3320d00a1
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2021-10-06 19:19:18 +0530

    navigation bar is done with responsiveness and animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
---
+++ src/Components/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import LogoBanner from "./Home/LogoBanner";
+
+const Home = () => {
+  return (
+    <Container>
+      <Header />
+      <LogoBanner />
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -10;
+    background-image: url("/images/home-background.png");
+    background-size: cover;
+    background-repeat: none;
+    background-position: top;
+  }
+`;
